d5fca708cede85c964b5187797dd33c81c4bc52b
Jack Rostron <jackrostron@powa.com>
2014-05-15 18:52:19 +0100

    Added project

---
+++ UIAutomation/UIAutomation/IATAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  IATAppDelegate.m
+//  UIAutomation
+//
+//  Created by Jack Rostron on 15/05/2014.
+//  Copyright (c) 2014 Jack Rostron. All rights reserved.
+//
+
+#import "IATAppDelegate.h"
+
+@implementation IATAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ UIAutomation/UIAutomation/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  UIAutomation
+//
+//  Created by Jack Rostron on 15/05/2014.
+//  Copyright (c) 2014 Jack Rostron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
