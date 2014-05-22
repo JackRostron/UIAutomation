111a8a6f764649299c661ce8485e82afdfa379bb
Jack Rostron <jackrostron@powa.com>
2014-05-22 15:35:03 +0100

    Objective-C can now communicate to Instruments

--- JavaScriptCommunicator/Bash/test.js
+++ JavaScriptCommunicator/Bash/test.js
@@ -1,2 +1,18 @@
 var target = UIATarget.localTarget();
-target.host().performTaskWithPathArgumentsTimeout("/bin/bash", ["/Users/JackRostron/Downloads/commobjc.sh"], 999);
+
+//Communicate to Objective-C
+//target.host().performTaskWithPathArgumentsTimeout("/bin/bash", ["/Users/JackRostron/Downloads/commobjc.sh"], 999);
+
+//Listen back from Objective-C
+var file_contents = target.host().performTaskWithPathArgumentsTimeout("/bin/cat", ["./wibble.txt"], 90);
+
+if (file_contents.stderr) {
+	UIALogger.logMessage("ERROR");
+} else {
+	UIALogger.logMessage(file_contents.stdout);
+}
+
+if (file_contents.stdout == "ListTree") {
+	target.logElementTree();
+	target.host().performTaskWithPathArgumentsTimeout("/bin/bash", ["/Users/JackRostron/Downloads/commobjc.sh"], 3);
+}
--- JavaScriptCommunicator/JavaScriptCommunicator/IATAppDelegate.m
+++ JavaScriptCommunicator/JavaScriptCommunicator/IATAppDelegate.m
@@ -8,17 +8,52 @@
 
 #import "IATAppDelegate.h"
 
+typedef enum {
+    kInstrumentsCommandListTree,
+} kInstrumentsCommand;
+
+
+@interface IATAppDelegate ()
+//@property (nonatomic, strong)
+@end
+
+
 @implementation IATAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     [[NSAppleEventManager sharedAppleEventManager] setEventHandler:self andSelector:@selector(handleURLEvent:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
+    
+    [self sendCommandToInstruments:kInstrumentsCommandListTree];
 }
 
 - (void)handleURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 {
     NSString *url = [[event paramDescriptorForKeyword:keyDirectObject] stringValue];
     NSLog(@"%@", url);
+}
+
+- (BOOL)sendCommandToInstruments:(kInstrumentsCommand)command
+{
+    NSString *commandString;
+    
+    switch (command) {
+        case kInstrumentsCommandListTree:
+            commandString = @"ListTree";
+            break;
+            break;
+    }
+    
+    if (commandString) {
+        NSError *error;
+        
+        ///Users/JackRostron/Desktop/UIAutomation/JavaScriptCommunicator/Bash/wibble.txt
+        
+        [commandString writeToFile:@"/Users/JackRostron/Desktop/UIAutomation/JavaScriptCommunicator/Bash/wibble.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+        return (error) ? NO : YES;
+    }
+    
+    return NO;
 }
 
 @end
