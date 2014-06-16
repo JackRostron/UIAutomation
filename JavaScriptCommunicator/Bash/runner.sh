061e25055348bbec915e5cd896bd18650dc5df9b
Jack Rostron <Jack@Jacks-MacBook-Pro.local>
2014-06-16 18:41:58 +0100

    Removed all unused projects and duplicate files. Relocated Javascript files I modified (Sam created but needed id's Xcode replaces at runtime) to Javascript directory in folder titled "Xcode". Legacy directory contains original UIAutomation project that uses classes and can monitor USB. ScreenshotPreview is Mavericks compatible program. Recreating project in Swift and titled "iOS UIAutomator". Will make compatible with Mavericks/Yosemite & iOS6/7/8. GitHub issues now relate to this project. Issues raised with existing project will be marked. Milestones now relate to Swift releases.

--- JavaScriptCommunicator/Bash/runner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-instruments -t /Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate /Users/JackRostron/Library/Developer/Xcode/DerivedData/PowaTag-beyjcveresnrxbcuyxgpplxujlnh/Build/Products/QA-iphonesimulator/PowaTag-Code-Coverage.app -e UIASCRIPT ./test.js -e UIARESULTSPATH '~/Desktop/Results/'
-#>> ./raw_results.txt
