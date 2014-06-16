0c364ba6dcc2d08514e94eaccc6a3ae0514dfd69
Jack Rostron <Jack@Jacks-MacBook-Pro.local>
2014-06-16 18:49:12 +0100

    Created Swift project

---
+++ Swift/iOS-UIAutomator/iOS-UIAutomatorTests/iOS_UIAutomatorTests.swift
@@ -0,0 +1,35 @@
+//
+//  iOS_UIAutomatorTests.swift
+//  iOS-UIAutomatorTests
+//
+//  Created by Jack Rostron on 16/06/2014.
+//  Copyright (c) 2014 Jack Rostron. All rights reserved.
+//
+
+import XCTest
+
+class iOS_UIAutomatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
